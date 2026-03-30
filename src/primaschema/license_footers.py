f193863f338cecbc4d2d61fab6c202a8f9c91cd6
ChrisKent <chrisgkent@gmail.com>
2026-03-30 13:00:52 +0100

    feat: add LICENSE_FOOTERS for all CC licenses
    Maps every SchemeLicense member to a Markdown footer containing a
    separator, the canonical CC license URL, and the CC badge image.

---
+++ src/primaschema/license_footers.py
@@ -0,0 +1,39 @@
+from primaschema.schema.info import SchemeLicense
+
+LICENSE_FOOTERS: dict[SchemeLicense, str] = {
+    SchemeLicense.CC0_1FULL_STOP0: """\n\n------------------------------------------------------------------------
+
+This work has been dedicated to the public domain under [CC0 1.0 Universal](https://creativecommons.org/publicdomain/zero/1.0/).
+
+![](https://licensebuttons.net/p/zero/1.0/88x31.png)""",
+    SchemeLicense.CC_BY_4FULL_STOP0: """\n\n------------------------------------------------------------------------
+
+This work is licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
+
+![](https://i.creativecommons.org/l/by/4.0/88x31.png)""",
+    SchemeLicense.CC_BY_SA_4FULL_STOP0: """\n\n------------------------------------------------------------------------
+
+This work is licensed under a [Creative Commons Attribution-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-sa/4.0/).
+
+![](https://i.creativecommons.org/l/by-sa/4.0/88x31.png)""",
+    SchemeLicense.CC_BY_NC_4FULL_STOP0: """\n\n------------------------------------------------------------------------
+
+This work is licensed under a [Creative Commons Attribution-NonCommercial 4.0 International License](http://creativecommons.org/licenses/by-nc/4.0/).
+
+![](https://i.creativecommons.org/l/by-nc/4.0/88x31.png)""",
+    SchemeLicense.CC_BY_NC_SA_4FULL_STOP0: """\n\n------------------------------------------------------------------------
+
+This work is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/).
+
+![](https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png)""",
+    SchemeLicense.CC_BY_ND_4FULL_STOP0: """\n\n------------------------------------------------------------------------
+
+This work is licensed under a [Creative Commons Attribution-NoDerivatives 4.0 International License](http://creativecommons.org/licenses/by-nd/4.0/).
+
+![](https://i.creativecommons.org/l/by-nd/4.0/88x31.png)""",
+    SchemeLicense.CC_BY_NC_ND_4FULL_STOP0: """\n\n------------------------------------------------------------------------
+
+This work is licensed under a [Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License](http://creativecommons.org/licenses/by-nc-nd/4.0/).
+
+![](https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png)""",
+}
